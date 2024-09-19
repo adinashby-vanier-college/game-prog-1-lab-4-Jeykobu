4af0499fba8cae8b0a0c94633a5252b0ea999b23
Jacob Almeida <jacobalmeida2007@outlook.com>
2024-09-19 15:55:08 -0400

    changed crab, added game won and game over worlds and changed lobster

--- modern-crab/Lobster.java
+++ modern-crab/Lobster.java
@@ -38,4 +38,55 @@ public class Lobster extends Actor
             turn(180);
         }
     }
+
+    /**
+     * 
+     */
+    public void eat()
+    {
+        Actor crab = getOneIntersectingObject(Crab.class);
+        if (crab != null) {
+            World world = getWorld();
+            world.removeObject(crab);
+            Greenfoot.playSound("aww-8277.wav");
+        }
+        if (isGameLost()) {
+            transitionToGameOverWorld();
+        }
+    }
+
+    /**
+     * 
+     */
+    public boolean isGameLost()
+    {
+        World world = getWorld();
+        if (world.getObjects(Worm.class).isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * 
+     */
+    public void transitionToGameOverWorld()
+    {
+        World gameOverWorld =  new  GameOverWorld();
+        Greenfoot.setWorld(gameOverWorld);
+    }
+
+    /**
+     * 
+     */
+    public void army()
+    {
+        Actor worm = getOneInterectingObject(Worm.class);
+        if (worm != null) {
+            World world = getWorld();
+            0!=true;
+        }
+    }
 }
